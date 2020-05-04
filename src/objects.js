d5c54d7e3fa2aec419caab50309a780587389bd9
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-05-04 12:24:29 +0100

    finished 62 puzzle tests

--- src/objects.js
+++ src/objects.js
@@ -1,39 +1,56 @@
 const createPerson = (name, age) => {
-  
+  return { name, age };
+};
+
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  let sum = 0;
+  let i = 0;
+  for (i = 0; i < people.length; i++) {
+    const person = people[i].age;
+    sum += person;
+  }
+  return sum / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age,
+    introduce(friendName) {
+      return `Hi ${friendName}, my name is ${this.name} and I am ${this.age}!`
+    }
+  };
 };
 
 module.exports = {
